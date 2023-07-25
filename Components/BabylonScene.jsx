42aeba2836ee3803865919e1c9da5260eb0297cf
UTTEJ K <uttej09092001@gmail.com>
2023-07-25 11:35:22 +0530

    v001

--- Components/BabylonScene.jsx
+++ Components/BabylonScene.jsx
@@ -3,6 +3,7 @@ import { useEffect, useRef } from "react";
 import {
   ArcRotateCamera,
   Color3,
+  Color4,
   Engine,
   HemisphericLight,
   Mesh,
@@ -10,9 +11,11 @@ import {
   PolygonMeshBuilder,
   Scene,
   StandardMaterial,
+  Vector2,
   Vector3,
   VertexData,
 } from "@babylonjs/core";
+import * as earcut from "earcut";
 
 const BabylonScene = () => {
   const sceneRef = useRef(null);
@@ -32,6 +35,7 @@ const BabylonScene = () => {
     camera.position = new Vector3(0, 10, -10);
     camera.attachControl(sceneRef.current, true);
     const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
+    light.intensity = 0.9;
     const ground = MeshBuilder.CreateGround(
       "ground",
       { width: 6, height: 6 },
@@ -39,20 +43,16 @@ const BabylonScene = () => {
     );
     const groundmat = new StandardMaterial("groundMat", scene);
     groundmat.diffuseColor = new Color3(0.1, 0.1, 0.1);
+    groundmat.alpha = 0.1;
     ground.material = groundmat;
 
-    const customMesh = new Mesh("Custom", scene);
-    const vertexData = new VertexData();
     const mat = new StandardMaterial("mat", scene);
     mat.emissiveColor = Color3.Green();
     mat.backFaceCulling = false;
     mat.twoSidedLighting = true;
-    customMesh.material = mat;
 
     const bufferMeshes = [];
     const positions = [];
-    const indices = [];
-    let i = 0;
 
     scene.onPointerDown = (evt) => {
       // pick mesh at the pointer position
@@ -61,8 +61,6 @@ const BabylonScene = () => {
       // console.log(evt.button);
 
       if (hit.faceId != -1) {
-        const initialPos = hit.pickedPoint;
-
         if (evt.button == 0) {
           const Mesh = MeshBuilder.CreateSphere(
             "PlaceHolder",
@@ -72,33 +70,24 @@ const BabylonScene = () => {
           try {
             Mesh.position = hit.pickedPoint;
             bufferMeshes.push(Mesh);
-            positions.push(Mesh.position._x);
-            positions.push(Mesh.position._y);
-            positions.push(Mesh.position._z);
-            indices.push(i);
-            indices.push(i + 1);
-            indices.push(i + 2);
-            // indices.push(i);
-            // indices.push(i + 2);
-            // indices.push(i + 3);
-            // indices[positions.length] = 0;
-            i += 1;
+            positions.push(new Vector2(Mesh.position._x, Mesh.position._z));
           } catch (error) {
             console.error(error);
           }
         }
-        // positions[positions.length] = initialPos._x;
-        // positions[positions.length + 1] = initialPos._y;
-        // positions[positions.length + 1] = initialPos._z;
-
         if (evt.button == 2) {
-          console.log(positions, indices);
-          vertexData.positions = positions;
-          vertexData.indices = indices;
-          vertexData.applyToMesh(customMesh);
-          for (let i = 0; i < bufferMeshes.length; i++) {
+          console.log(positions);
+          const poly_tri = new PolygonMeshBuilder(
+            "polygon",
+            positions,
+            scene,
+            earcut
+          );
+          const polygon = poly_tri.build();
+          polygon.material = mat;
+          const len = bufferMeshes.length;
+          for (let i = 0; i < len; i++) {
             bufferMeshes[i].dispose();
-            // bufferMeshes.length = 0;
           }
         }
       }
